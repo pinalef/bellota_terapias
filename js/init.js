72bf1995179d104b3b8b5e03cf2b46d6073c048a
pinalef <tierralavista@gmail.com>
2020-04-08 20:52:40 -0400

    fixed nav

--- js/init.js
+++ js/init.js
@@ -1,9 +1,11 @@
 (function($) {
   $(function() {
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-    $(".dropdown-trigger").dropdown({ hover: true, belowOrigin: false });
+    $('.dropdown-trigger').dropdown({ // Does not change width of dropdown to that of the activator
+      hover: true,
+      coverTrigger: false
+
+    });
     $('.materialboxed').materialbox();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
