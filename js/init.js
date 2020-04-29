ab843cedac3c82dc9bc244c34726359bf426da39
Pamela Inalef <tierralavista@gmail.com>
2020-04-29 01:20:13 -0400

    changing main  content

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
-(function($) {
-  $(function() {
+(function ($) {
+  $(function () {
+    $('.parallax').parallax();
     $('.sidenav').sidenav();
     $('.dropdown-trigger').dropdown({ // Does not change width of dropdown to that of the activator
       hover: true,
@@ -7,5 +8,8 @@
 
     });
     $('.materialboxed').materialbox();
+    $('.scrollspy').scrollSpy();
+    $('.carousel').carousel();
+    $('.slider').slider();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
